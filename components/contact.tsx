70c678f1422baeecf3a1902c89149b10db4dcb02
abkGami <yahabubakar1@gmail.com>
2025-05-30 21:54:52 +0100

    working on contacts

--- components/contact.tsx
+++ components/contact.tsx
@@ -27,7 +27,7 @@ export default function Contact() {
         </motion.div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-          {/* Contact Form */}
+          Contact Form
           <motion.div
             initial={{ opacity: 0, x: 50 }}
             whileInView={{ opacity: 1, x: 0 }}
