a5880229fcf8fb9e4dac4e45e0946911f19df4d6
abkGami <yahabubakar1@gmail.com>
2025-06-25 13:57:24 +0100

    stay

--- app/boat-trip/page.tsx
+++ app/boat-trip/page.tsx
@@ -276,7 +276,11 @@ export default function BoatsPage() {
     <div className="min-h-screen bg-gradient-to-b from-sky-50 to-blue-50">
       <Navigation />
       {/* Search Card */}
-      <CustomerSLideshow />
+      {
+        // !loading
+        !hasSearched ? <CustomerSLideshow /> : <div></div>
+      }
+
       <section className="pt-24 pb-8 flex justify-center">
         <Card className="w-full max-w-4xl mx-auto shadow-2xl border-0 p-8">
           <CardHeader>
--- components/boatTrip-slideshow.tsx
+++ components/boatTrip-slideshow.tsx
@@ -146,9 +146,9 @@ export default function CustomerSLideshow() {
     <section className="py-20 bg-gradient-to-b from-gray-50 to-white mt-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
+          //   initial={{ opacity: 0, y: 50 }}
+          //   whileInView={{ opacity: 1, y: 0 }}
+          //   transition={{ duration: 0.8 }}
           className="text-center mb-16"
         >
           <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
