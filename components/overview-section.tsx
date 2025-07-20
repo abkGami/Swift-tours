a4d810ac3d499dbc1ce56ca35c859e369e2bf4a1
abkGami <yahabubakar1@gmail.com>
2025-07-20 17:29:42 +0100

    new minor changes

--- components/overview-section.tsx
+++ components/overview-section.tsx
@@ -91,7 +91,7 @@ export default function OverviewSection() {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {overviewCards.map((card, index) => {
             const IconComponent = card.icon;
             return (
